c3c0dc2ac3bfd45b40e626a54237a74a5c8a320e
Brian Takita <brian.takita@gmail.com>
2021-05-28 05:44:52 -0400

    update dependencies

--- src/_obj_key_hash.ts
+++ src/_obj_key_hash.ts
@@ -1,7 +1,9 @@
 /**
  * Returns Hash of each `value[key]` in `obj`.
  */
-export function _obj_key_hash<T extends unknown = unknown>(obj:T, key:string) {
+export function _obj_key_hash</*@formatter:off*/
+	T extends unknown = unknown
+/*@formatter:on*/>(obj:T, key:string) {
 	const obj_key_hash = {} as Record<string, T[keyof T]>
 	for (let attr_key in obj) {
 		obj_key_hash[attr_key] = ((obj as any)[attr_key] && (obj as any)[attr_key])[key]
