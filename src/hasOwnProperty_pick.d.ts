33b9534ade786a4ba3dda8d96037093f63c0072d
Brian Takita <brian.takita@gmail.com>
2022-01-07 21:56:50 -0600

    formatting

--- src/hasOwnProperty_pick.d.ts
+++ src/hasOwnProperty_pick.d.ts
@@ -2,5 +2,5 @@
  * Returns object with picked values,
  * not including including inherited property values (i.e. if hasOwnProperty is false).
  */
-export declare function hasOwnProperty_pick<I extends unknown = unknown, O = I>(obj: I, ...key_a: string[]): O | undefined;
-export { hasOwnProperty_pick as pick__hasOwnProperty };
+export declare function hasOwnProperty_pick<I extends unknown = unknown, O = I>(obj:I, ...key_a:string[]):O|undefined;
+export { hasOwnProperty_pick as pick__hasOwnProperty }
