8c71465b49fca98572735ef0d9c491c87bb3c745
Brian Takita <brian.takita@gmail.com>
2023-02-23 13:55:23 -0500

    formatting

--- src/be_/index.d.ts
+++ src/be_/index.d.ts
@@ -3,25 +3,45 @@ export declare const pending_symbol:unique symbol
  * Returns a function to ensure that a member label is defined on a ctx object,
  * otherwise it creates the value using the val_ factory function.
  */
-export declare function be_<Val extends NonNullable<unknown>, ctx_T extends Ctx = Ctx>(
+export declare function be_<
+	Val extends NonNullable<unknown>,
+	ctx_T extends Ctx = Ctx
+>(
 	val_:be__val__T<Val, ctx_T>, be__opts?:be__opts_T<Val>
 ):Be<Val, ctx_T>
-export declare function be_<Val extends NonNullable<unknown>, ctx_T extends Ctx = Ctx>(
+export declare function be_<
+	Val extends NonNullable<unknown>,
+	ctx_T extends Ctx = Ctx
+>(
 	label:string, val_:be__val__T<Val, ctx_T>, be__opts?:be__opts_T<Val>
 ):Be<Val, ctx_T>
-export declare function be__set<Val extends NonNullable<unknown>, ctx_T extends Ctx = Ctx>(
+export declare function be__set<
+	Val extends NonNullable<unknown>,
+	ctx_T extends Ctx = Ctx
+>(
 	ctx:Ctx, be:Be<Val>, val:Val
 ):MapCtx
-export declare function be__delete<Val extends NonNullable<unknown>>(argv__ctx:Ctx, be:Be<Val>):void
+export declare function be__delete<
+	Val extends NonNullable<unknown>
+>(argv__ctx:Ctx, be:Be<Val>):void
 export declare type MapCtx = Map<Be<any>|string|symbol, unknown>
 export interface NestedMapCtx extends Array<NestedMapCtx|MapCtx> {}
 export type Ctx = MapCtx|NestedMapCtx
-export declare type Be<Val extends NonNullable<unknown>, ctx_T extends Ctx = Ctx> =
-	(ctx:ctx_T, opts?:be_opts_T)=>Val
-export declare type B<Val extends NonNullable<unknown>, ctx_T extends Ctx = Ctx> = Be<Val, ctx_T>
-export declare type be__val__T<Val extends NonNullable<unknown>, ctx_T extends Ctx = Ctx> =
-	(ctx:ctx_T, label:Be<Val, ctx_T>, opts?:be_opts_T)=>Val
-export interface be__opts_T<Val extends NonNullable<unknown>> {
+export declare type Be<
+	Val extends NonNullable<unknown>,
+	ctx_T extends Ctx = Ctx
+> = (ctx:ctx_T, opts?:be_opts_T)=>Val
+export declare type B<
+	Val extends NonNullable<unknown>,
+	ctx_T extends Ctx = Ctx
+> = Be<Val, ctx_T>
+export declare type be__val__T<
+	Val extends NonNullable<unknown>,
+	ctx_T extends Ctx = Ctx
+> = (ctx:ctx_T, label:Be<Val, ctx_T>, opts?:be_opts_T)=>Val
+export interface be__opts_T<
+	Val extends NonNullable<unknown>
+> {
 	is_source_?(map_ctx:MapCtx, ctx:Ctx):boolean
 }
 export interface be_opts_T {
